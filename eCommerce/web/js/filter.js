13d95c2e08033f76c8d09213c83efdf632aea64f
selim <selim@DESKTOP-UDRA5SR>
2016-06-07 18:03:39 +0200

    consegna M4

--- eCommerce/web/js/filter.js
+++ eCommerce/web/js/filter.js
@@ -4,107 +4,6 @@
  * and open the template in the editor.
  */
 
-
- /*jQuery(document).ready(function($){
-    $('input#filtra').on('keyup',function(){
-        var pattern = $('input#filtra').val(); //prendiamo il valore della nostro campo input
-        document.getElementById("avviso").innerHTML="Hai scritto " + pattern;
-        
-        $.ajax({
-            url: "filter.json",
-            data: {
-                q: pattern
-            },
-            dataType: 'json',
-            success: function (data, state) {
-                 popola(data);
-            },
-            error: function (data, state) {
-                //alert("Chiamata fallita!!!");
-            }
-        });
-    });
-});
-
-function popola(data){
-    
-    
-}*/
-
-/*$(document).ready(function ()
-{
-   $("#filtra").keyup(function()
-    {  
-        // Preleva il valore
-         var text = $("#filtra").val(); 
-       
-        $.ajax(
-        {
-            url: "filter.json",
-            data:{
-              cmd: "search",
-              text: text
-            },
-            dataType: 'json',
-            success : function(data, state){
-                popolaTabella(data);
-            },
-            error : function(data, state){
-            }
-        });
-        
-       // Funzione che viene richiamata in caso di successo
-        function popolaTabella(listaAuto)
-        {
-            // Cancella la lista
-            $("tr:not(.intestazione)").remove();
-            
-            
-            // Per ogni alunno trovato dal database
-            for(var oggetto in listaAuto)
-            {
-                  document.writeln("Nome " + oggetto.nomeAuto +  "<br/>");
-               /* var tr = document.createElement("tr");
-                
-                var td = document.createElement("td");
-                var txt = document.createTextNode(oggetto.nomeAuto);
-                td.appendChild(txt);
-                tr.appendChild(td);
-
-                var td = document.createElement("td");
-                var x = document.createElement("IMG");
-                x.setAttribute("src", oggetto.url);
-                x.setAttribute("alt", "Foto di " + oggetto.nome);
-                td.appendChild(x);
-                tr.appendChild(td);
-
-                var td = document.createElement("td");
-                var txt = document.createTextNode(oggetto.quantita);
-                td.appendChild(txt);
-                tr.appendChild(td);
-
-                var td = document.createElement("td");
-                var txt = document.createTextNode(oggetto.prezzo);
-                td.appendChild(txt);
-                tr.appendChild(td);
-
-                var td = document.createElement("td");
-                var a = document.createElement("A");
-                a.setAttribute("href", "cliente.html");
-                var txt = document.createTextNode("Aggiungi");
-                a.appendChild(txt);
-                td.appendChild(a);
-                tr.appendChild(td);
-
-
-                document.getElementById("tabella").appendChild(tr);
-            }
-        }
-    }); 
-});*/
-
-
-
 $(document).ready(function ()
 {
    $("#filtra").keyup(function()
@@ -164,11 +63,14 @@ $(document).ready(function ()
                 var td = document.createElement("td");
                 var a = document.createElement("A");
                 a.setAttribute("href", "cliente.html?idAuto="+ listaAuto[oggetto].id);
-                var txt = document.createTextNode("Aggiungi");
-                a.appendChild(txt);
+                var x = document.createElement("IMG");
+                x.setAttribute("src", "Images/aggiungi.png");
+                x.setAttribute("alt", "Aggiungi");
+                x.setAttribute("width", 40);
+                x.setAttribute("height", 40);
+                a.appendChild(x);
                 td.appendChild(a);
                 tr.appendChild(td);
-
 
                 document.getElementById("tabella").appendChild(tr);
             } 
